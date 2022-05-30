403346cedfa6a754e96aba0fcffcc9e6bafeef6f
Vladislavs Bužinskis <intelifoxgames@gmail.com>
2022-05-30 15:01:51 +0300

    Add missing docstrings

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -10,6 +10,7 @@ from app_factory import create_app
 
 @pytest.fixture
 def client():
+    """Launches test app client."""
     app = create_app()
     app.config["TESTING"] = True
     with app.test_client() as client:
@@ -17,10 +18,12 @@ def client():
 
 
 def test_hackernews(client):
+    """Tests the successful response from the HackerNews webpage."""
     response = requests.get("https://news.ycombinator.com")
     assert response.status_code == 200
 
 
 def test_main_page(client):
+    """Tests the successful response from the Hacker-View main page."""
     response = client.get("/")
     assert response.status_code == 200
